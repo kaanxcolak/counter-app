27e8c130852a19acf9d5e6f553323acb20b7ca5b
Kaan Colak <kaancolak3458@gmail.com>
2022-06-15 11:47:41 +0300

    added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {store} from "./redux/store";
+import {Provider} from "react-redux"; //sarmaladığı componentteki dataları paslar
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.creat1eRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Provider store={store}>
+  <App />
+  </Provider>
+    
   </React.StrictMode>
 );
 
